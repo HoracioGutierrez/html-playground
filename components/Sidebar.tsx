cf98beb157f53496564bf5213dac08d7e76a1ce1
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-09-02 20:55:47 -0300

    Add more text to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { Button } from "./ui/button";
-import { X } from "lucide-react";
+import { CheckCircleIcon, Copy, Edit, X } from "lucide-react";
 import { motion } from "framer-motion"
 import { cn, SidebarProps } from "@/lib/utils";
 import { useSidebarStore } from "@/stores/SidebarStore";
@@ -25,10 +25,11 @@ function Sidebar({ className = "" }: SidebarProps) {
         <div className="flex flex-col gap-4 mb-6 text-fluid-md dark:text-secondary-foreground">
           <p>This playground was created with the intention of having a tool for beginners to better learn HTML nesting and the structure of the DOM. You can drag and drop elements to the DOM and see how they nest.</p>
           <p>In case you try to nest an element inside another element that doesn&apos;t allow it, the playground will show you an error message. You will also get an error message if you exceed the nesting limit for that particular element.</p>
-          <p>You can start by dragging the yellow badges on the right side of the screen and dropping them into the area that says <span className="font-thin italic"> &apos;index.html&apos;</span>.</p>
-          <p>Once you have nested at least one element, a button that says <span className="font-thin italic">&apos;generate html&apos;</span> will appear. Clicking on it will generate the HTML code for the elements you have nested and copy it to your clipboard automatically.</p>
+          <p>You can start by dragging the light blue badges on the right side of the screen and dropping them into the area that says <span className="font-thin italic"> &apos;{"<!DOCTYPE html>"}&apos;</span>.</p>
+          <p>Once you have nested at least one element, a button that looks like this <Copy className="inline-block mx-2"/> will be available. Clicking on it will generate the HTML code for the elements you have nested and copy it to your clipboard automatically.</p>
+          <p>On top, you can find a button that looks like this <CheckCircleIcon className="inline-block mx-2"/>. Clicking on it try to validate the elements according to the BEM standard. If the validation is successful, the button will turn green, otherwise it will turn red displaying the errors that were found.</p>
           <p>You can also remove elements by clicking on the <span className="font-thin italic">&apos;x&apos;</span> icons that appears on the top right corner of the element you want to remove.</p>
-          <p>Also, you can edit the attributes of the elements by clicking on the <span className="font-thin italic">&apos;edit&apos;</span> icons that appears on the top right corner of the element you want to edit. A popup will appear with the attributes of the element, you can edit them and click on the <span className="font-thin italic">&apos;save&apos;</span> button to save the changes.</p>
+          <p>Also, you can edit the attributes of the elements by clicking on the <Edit className="inline-block mx-2"/> icons that appears on the top right corner of the element you want to edit. A popup will appear with the attributes of the element, you can edit them and click on the <span className="font-thin italic">&apos;save&apos;</span> button to save the changes.</p>
         </div>
         <p className="font-bold text-center text-primary text-xl">Enjoy! 🚀</p>
         <Button className="xl:hidden bg-primary drop-shadow-md shadow-md mt-auto w-full text-xl uppercase" onClick={closeSidebar}>
